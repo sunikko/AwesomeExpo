1007ac32d504aff4b7af96715b2de9a25e635464
Soy <soyeon.won@oxbridgeuk.com>
2025-03-02 05:34:02 +0000

    urlText :

--- app/browser.tsx
+++ app/browser.tsx
@@ -1,6 +1,6 @@
 import { useLocalSearchParams } from "expo-router";
-import React from "react";
-import { Platform, SafeAreaView, StyleSheet, Text } from "react-native";
+import React, { useMemo, useState } from "react";
+import { Platform, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import WebView from "react-native-webview";
 
 const styles = StyleSheet.create({
@@ -9,15 +9,37 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "black",
   },
+  urlContainer: {
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 5,
+  },
+  urlText: {
+    color: "white",
+  },
 });
 
 const BrowserScreen = () => {
   const params = useLocalSearchParams();
   const initialUrl = params.initialUrl as string;
+  const [url, setUrl] = useState(initialUrl);
+  const urlTitle = useMemo(
+    () => url.replace("https://", "").split("/")[0],
+    [url],
+  );
 
   return (
     <SafeAreaView style={styles.safearea}>
-      <WebView source={{ uri: initialUrl }}></WebView>
+      <View style={styles.urlContainer}>
+        <Text style={styles.urlText}>{urlTitle}</Text>
+      </View>
+      <WebView
+        source={{ uri: initialUrl }}
+        onNavigationStateChange={(event) => {
+          setUrl(event.url);
+        }}
+      ></WebView>
     </SafeAreaView>
   );
 };
