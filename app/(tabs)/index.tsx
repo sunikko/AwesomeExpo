abf415a7b6c23689235160d6fd22ddf83252fa74
Soy <soyeon.won@oxbridgeuk.com>
2025-03-02 05:19:17 +0000

    homescreen: routing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,46 @@
+import { useRouter } from "expo-router";
 import React from "react";
-import { Text } from "react-native";
+import { Platform, SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import WebView from "react-native-webview";
+
+const styles = StyleSheet.create({
+  safearea: {
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    flex: 1,
+  },
+});
 
 const HomeScreen = () => {
-  return <Text>Home</Text>;
+  const router = useRouter();
+
+  return (
+    <SafeAreaView style={styles.safearea}>
+      <WebView
+        source={{ uri: "https://m.naver.com" }}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+        onShouldStartLoadWithRequest={(request) => {
+          console.log("Home - request", request);
+
+          if (
+            request.url.startsWith("https://m.naver.com") ||
+            request.mainDocumentURL?.startsWith("https://m.naver.com")
+          ) {
+            return true;
+          }
+
+          if (request.url != null && request.url.startsWith("https://")) {
+            router.navigate({
+              pathname: "browser",
+            });
+            return false;
+          }
+
+          return true;
+        }}
+      ></WebView>
+    </SafeAreaView>
+  );
 };
 
 export default HomeScreen;
